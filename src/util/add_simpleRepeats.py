f24774a55038e942cb7ff0471fc7c8c85b99e146
Dominic Boceck <dboceck@gmail.com>
2020-02-06 17:14:15 +0100

    Simplify code

--- src/util/add_simpleRepeats.py
+++ src/util/add_simpleRepeats.py
@@ -32,7 +32,7 @@ def extract_simple_repeats(row, current_repeat_table):
     
 
 def extract_data(data, current_repeat_table):
-    data[["SimpleTandemRepeatRegion","SimpleTandemRepeatLength"]] = data.apply(lambda x: pd.Series(extract_simple_repeats(x, current_repeat_table)), axis=1)
+    data[["SimpleTandemRepeatRegion", "SimpleTandemRepeatLength"]] = data.apply(lambda x: pd.Series(extract_simple_repeats(x, current_repeat_table)), axis=1)
     
     return data
 
@@ -40,85 +40,14 @@ def extract_data(data, current_repeat_table):
 def group_and_process_data(repeat_data, data):
     repeats = pd.read_csv(repeat_data, sep="\t", low_memory=False, header=None)
     repeats.set_index([1], inplace=True)
-
-    repeats_chr1 = repeats.loc["chr1"]
-    repeats_chr2 = repeats.loc["chr2"]
-    repeats_chr3 = repeats.loc["chr3"]
-    repeats_chr4 = repeats.loc["chr4"]
-    repeats_chr5 = repeats.loc["chr5"]
-    repeats_chr6 = repeats.loc["chr6"]
-    repeats_chr7 = repeats.loc["chr7"]
-    repeats_chr8 = repeats.loc["chr8"]
-    repeats_chr9 = repeats.loc["chr9"]
-    repeats_chr10 = repeats.loc["chr10"]
-    repeats_chr11 = repeats.loc["chr11"]
-    repeats_chr12 = repeats.loc["chr12"]
-    repeats_chr13 = repeats.loc["chr13"]
-    repeats_chr14 = repeats.loc["chr14"]
-    repeats_chr15 = repeats.loc["chr15"]
-    repeats_chr16 = repeats.loc["chr16"]
-    repeats_chr17 = repeats.loc["chr17"]
-    repeats_chr18 = repeats.loc["chr18"]
-    repeats_chr19 = repeats.loc["chr19"]
-    repeats_chr20 = repeats.loc["chr20"]
-    repeats_chr21 = repeats.loc["chr21"]
-    repeats_chr22 = repeats.loc["chr22"]
-    repeats_chrX = repeats.loc["chrX"]
-    repeats_chrY = repeats.loc["chrY"]
     
     data_grouped = [group for key, group in data.groupby("Chr")]
 
     for group in data_grouped:
-        if group["Chr"].iloc[0] == "1":
-            current_repeat_table = repeats_chr1
-        elif group["Chr"].iloc[0] == "2":
-            current_repeat_table = repeats_chr2
-        elif group["Chr"].iloc[0] == "3":
-            current_repeat_table = repeats_chr3
-        elif group["Chr"].iloc[0] == "4":
-            current_repeat_table = repeats_chr4
-        elif group["Chr"].iloc[0] == "5":
-            current_repeat_table = repeats_chr5
-        elif group["Chr"].iloc[0] == "6":
-            current_repeat_table = repeats_chr6
-        elif group["Chr"].iloc[0] == "7":
-            current_repeat_table = repeats_chr7
-        elif group["Chr"].iloc[0] == "8":
-            current_repeat_table = repeats_chr8
-        elif group["Chr"].iloc[0] == "9":
-            current_repeat_table = repeats_chr9
-        elif group["Chr"].iloc[0] == "10":
-            current_repeat_table = repeats_chr10
-        elif group["Chr"].iloc[0] == "11":
-            current_repeat_table = repeats_chr11
-        elif group["Chr"].iloc[0] == "12":
-            current_repeat_table = repeats_chr12
-        elif group["Chr"].iloc[0] == "13":
-            current_repeat_table = repeats_chr13
-        elif group["Chr"].iloc[0] == "14":
-            current_repeat_table = repeats_chr14
-        elif group["Chr"].iloc[0] == "15":
-            current_repeat_table = repeats_chr15
-        elif group["Chr"].iloc[0] == "16":
-            current_repeat_table = repeats_chr16
-        elif group["Chr"].iloc[0] == "17":
-            current_repeat_table = repeats_chr17
-        elif group["Chr"].iloc[0] == "18":
-            current_repeat_table = repeats_chr18
-        elif group["Chr"].iloc[0] == "19":
-            current_repeat_table = repeats_chr19
-        elif group["Chr"].iloc[0] == "20":
-            current_repeat_table = repeats_chr20
-        elif group["Chr"].iloc[0] == "21":
-            current_repeat_table = repeats_chr21
-        elif group["Chr"].iloc[0] == "22":
-            current_repeat_table = repeats_chr22
-        elif group["Chr"].iloc[0] == "X":
-            current_repeat_table = repeats_chrX
-        elif group["Chr"].iloc[0] == "Y":
-            current_repeat_table = repeats_chrY
+        if str(group["Chr"].iloc[0]) == "MT":
+            continue
         else:
-            print("ERROR: The file containing the repeat information seems to be broken!", file=sys.stderr)
+            current_repeat_table = repeats.loc["chr" + str(group["Chr"].iloc[0])]
         
         group = extract_data(group, current_repeat_table)
 
