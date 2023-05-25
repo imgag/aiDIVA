d2630186024573d5a6ea96ec9a1113c4301d1629
Dominic Boceck <dominic.boceck@gmail.com>
2023-05-25 11:52:32 +0200

    Remove unused imports

--- aidiva/run_annotation.py
+++ aidiva/run_annotation.py
@@ -1,17 +1,11 @@
 import argparse
-import helper_modules.combine_expanded_indels_and_create_csv as combine_expanded_indels
-import helper_modules.create_result_vcf as write_result
 import helper_modules.convert_indels_to_snps_and_create_vcf as expand_indels_and_create_vcf
-import helper_modules.convert_vcf_to_csv as convert_vcf
 import helper_modules.filter_vcf as filt_vcf
 import helper_modules.split_vcf_in_indel_and_snp_set as split_vcf
 import logging
 import os
-import pandas as pd
 import tempfile
 import time
-import variant_scoring.score_variants as predict
-import variant_prioritization.prioritize_variants as prio
 import variant_annotation.annotate_with_vep as annotate
 import yaml
 
