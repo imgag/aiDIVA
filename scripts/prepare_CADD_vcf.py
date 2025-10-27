c1c98f8b1d5e7f0a5bbf609db77b07f67b40fed7
Dominic Boceck <dboceck@gmail.com>
2025-10-27 15:51:29 +0100

    Fix VCF annotation key in preparation script to match the key in the VCF conversion

--- scripts/prepare_CADD_vcf.py
+++ scripts/prepare_CADD_vcf.py
@@ -11,7 +11,7 @@ outfile = sys.argv[2]
 
 with gzip.open(infile, "rt") as infile, open(outfile, "w") as writer:
     writer.write("##fileformat=VCFv4.1\n")
-    writer.write("##INFO=<ID=CADD_PHRED,Number=1,Type=String,Description=\"CADD phred score\">\n")
+    writer.write("##INFO=<ID=CADD,Number=1,Type=String,Description=\"CADD phred score\">\n")
     writer.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n")
 
     for line in infile:
@@ -21,7 +21,7 @@ with gzip.open(infile, "rt") as infile, open(outfile, "w") as writer:
         split = line.replace("\n", "").split("\t")
 
         if str(split[4]) != "":
-            writer.write(f"{split[0]}\t{split[1]}\t.\t{split[2]}\t{split[3]}\t.\t.\tCADD_PHRED={str(split[5])}\n")
+            writer.write(f"{split[0]}\t{split[1]}\t.\t{split[2]}\t{split[3]}\t.\t.\tCADD={str(split[5])}\n")
 
         else:
             print("WARNING: Skip missing CADD phred score!")
